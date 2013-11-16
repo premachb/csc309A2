5b3d79be47e56623c999d6b1efcffb49a5dfd372
Brien Premachandiran <premachb@gmail.com>
2013-11-16 15:55:58 -0500

    Added view for movie page

---
+++ uofttheater/application/views/main/movie.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h2>Showtimes for
+            <?php foreach($movie as $movie){
+                echo $movie->title;
+            } ?></h2>
+    </body>
+
+</html>
